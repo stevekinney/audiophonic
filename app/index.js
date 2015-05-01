9ba179894319fcb7afcbad12c6442228c0f24ce8
Steve Kinney <hello@stevekinney.net>
2015-04-30 22:45:34 -0600

    Setup basic synth

--- app/index.js
+++ app/index.js
@@ -1,6 +1,63 @@
-require('./stylesheets/main.scss');
+require('./stylesheets/main');
 
-import context from './lib/audio-context.js';
-import playMajorScale from './lib/major-scale-player.js';
+import $ from 'jquery';
+import context from './lib/audio-context';
+import notes from './lib/note-index';
 
-playMajorScale(context);
+const $currentFrequency = $('.current-frequency');
+const $currentVolume = $('.current-volume');
+const $frequencySlider = $('.frequency-slider');
+const $volumeSlider = $('.volume-slider');
+const $startButton = $('.start');
+const $stopButton = $('.stop');
+const $notes = $('.notes');
+
+let frequency = 220;
+
+const gainNode = (function () {
+  let gainNode = context.createGain();
+  gainNode.connect(context.destination);
+  gainNode.gain.value = 0;
+  return gainNode;
+}());
+
+const oscillator = (function () {
+  const oscillator = context.createOscillator();
+  oscillator.connect(gainNode);
+  oscillator.frequency.value = frequency;
+  oscillator.start();
+  return oscillator;
+}());
+
+const keyOfC = notes.filter(note => note.note.indexOf('/') === -1 ).slice(2, notes.length);
+
+keyOfC.forEach(note => {
+  $(`<button></button>`)
+    .addClass('note')
+    .data('frequency', note.frequency)
+    .text(note.note)
+    .appendTo($notes);
+});
+
+function adjustFrequency() {
+  oscillator.frequency.value = this.value;
+  $frequencySlider.val(this.value);
+  $currentFrequency.text(this.value);
+}
+
+function adjustVolume() {
+  gainNode.gain.value = this.value;
+  $volumeSlider.val(this.value);
+  $currentVolume.text(this.value * 10);
+}
+
+$('.note').on('mouseover', function () {
+  const frequency = parseInt($(this).data('frequency'), 10);
+  adjustFrequency.call({ value: frequency });
+});
+
+$('.notes').hover(adjustVolume.bind({ value: 1 }),
+                  adjustVolume.bind({ value: 0 }));
+
+$frequencySlider.on('input', adjustFrequency);
+$volumeSlider.on('input', adjustVolume);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   entry: './app/index.js',
   output: {
-    filename: 'bundle.js'       
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader' },
       { test: /\.scss$/, loader: "style!css!sass" }
     ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss']
   }
 };
