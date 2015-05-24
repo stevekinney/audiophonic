eeb7232bd78aef5f511a094f1931ff1530dc8a36
Steve Kinney <hello@stevekinney.net>
2015-05-23 22:31:24 -0600

    Restructure application

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app/index.js',
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "dist"),
     filename: 'bundle.js'
   },
   module: {
     loaders: [
       { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loader: "style!css!sass" }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.scss']
+    extensions: ['', '.js', '.json', '.scss', 'css']
   }
 };
